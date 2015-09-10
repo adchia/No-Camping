816fbd3e1f95b823a275d846c050dfae8f118640
Danny Chiao <adchia@Dannys-MacBook-Pro.local>
2015-09-10 10:36:14 -0400

    Adding some boilerplate for the this week view

---
+++ app/src/main/java/adchia/nocamping/InsightView.java
@@ -0,0 +1,16 @@
+package adchia.nocamping;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Controls a single insight for a user
+ */
+public class InsightView extends LinearLayout {
+
+  public InsightView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+}
